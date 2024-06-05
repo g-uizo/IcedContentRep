f44e39fec5dfe4328043396a848ea22a4fe468b3
João Siqueira <joaosiqueirapy@gmail.com>
2024-06-04 22:52:54 -0300

    new icon lib

--- resources/views/landingPage.blade.php
+++ resources/views/landingPage.blade.php
@@ -1,9 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-   <div class="p-5 flex flex-col gap-4 items-center justify-center">
+   <div class="p-5 flex flex-col gap-6 items-center justify-center">
         <p class="text-6xl text-center text-texto">Delicie-se com o melhor Sorvete Artesanal e mergulhe em uma experiência refrescante conosco!</p>
-        
+        <ion-icon name="snow" size="large"></ion-icon>
         <a href="{{ route('site.products')}}">
         <button class="py-3 px-6 bg-main rounded-lg ">Conheça nossos produtos</button>
         </a>
